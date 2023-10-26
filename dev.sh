08d26f23445da2cad2b9e45ccd582a61caa5a0e2
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-10-26 20:08:06 +0200

    + basic 'dev' environment initialization

---
+++ dev.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+if [[ $1 != 1 ]]; then
+  export DEV_MONGO_INITDB_ROOT_USERNAME=guest
+  export DEV_MONGO_INITDB_ROOT_PASSWORD=guest
+
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    sudo docker compose -f dev.docker-compose.yaml stop
+    sudo -E docker compose -f dev.docker-compose.yaml up -d
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    docker compose -f dev.docker-compose.yaml stop
+    docker compose -f dev.docker-compose.yaml up -d
+  fi
+fi
+
+if [ ! -f ./mongosh ]; then
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    curl https://downloads.mongodb.com/compass/mongosh-2.0.1-linux-x64.tgz --output mongosh.tgz
+    tar -zxvf mongosh.tgz
+
+    mv mongosh-2.0.1-linux-x64/bin/* ./
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    curl https://downloads.mongodb.com/compass/mongosh-2.0.1-darwin-arm64.zip --output mongosh.zip
+    unzip -o mongosh.zip
+
+    mv mongosh-2.0.1-darwin-arm64/bin/* ./
+  fi
+fi
+  
+if [[ $1 != 1 ]]; then
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    sleep 5s
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    sleep 5
+  fi
+fi
+
+chmod +x mongosh
+./mongosh mongodb://localhost:27000 --username guest --password guest --file ./db/drop.js --file ./db/organization_permission.js --file ./db/organization.js --file ./db/role.js --file ./db/member.js --file ./db/invitation_code.js
